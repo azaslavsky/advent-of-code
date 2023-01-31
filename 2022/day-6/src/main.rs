75ea3213221fa4ad347fb6d3c619358cb6b8bc0f
Alex Zaslavsky <>
2023-01-31 14:24:05 -0800

    [2022#6b] Solve prompt

--- 2022/day-6/src/main.rs
+++ 2022/day-6/src/main.rs
@@ -1,19 +1,26 @@
 use anyhow::{bail, Result};
-use common::get_input_file_lines_with_variant;
+use common::{get_input_file_lines_with_variant, Variant};
 use std::collections::HashSet;
 
-const MARKER_SIZE: usize = 4;
+const MARKER_SIZE_A: usize = 4;
+const MARKER_SIZE_B: usize = 14;
 
 fn main() -> Result<()> {
-    let (lines, _variant) = get_input_file_lines_with_variant()?;
+    let (lines, variant) = get_input_file_lines_with_variant()?;
     if lines.len() != 1 {
         bail!("input must be single line");
     }
+
+    let marker_size = match variant {
+        Variant::A => MARKER_SIZE_A,
+        Variant::B => MARKER_SIZE_B,
+    };
+
     lines[0]
         .chars()
         .collect::<Vec<char>>()
         .as_slice()
-        .windows(MARKER_SIZE)
+        .windows(marker_size)
         .enumerate()
         .take_while(|(index, window)| {
             let mut seen = HashSet::<char>::new();
@@ -22,7 +29,7 @@ fn main() -> Result<()> {
                     return true;
                 }
             }
-            println!("The first non-start-of-packer character is at: {}", index + MARKER_SIZE);
+            println!("The first non-start-of-packer character is at: {}", index + marker_size);
             return false;
         })
         .for_each(drop);
