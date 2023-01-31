a79d3d530e8decd8d3695d65148bfe8b52bffd96
Alex Zaslavsky <>
2023-01-30 18:40:08 -0800

    [2022#2b] Solve prompt

--- 2022/common/src/lib.rs
+++ 2022/common/src/lib.rs
@@ -5,8 +5,29 @@ use std::fs::File;
 pub fn open_input_file() -> Result<File> {
     let args: Vec<_> = std::env::args().collect();
     if args.len() != 2 {
-        bail!("Must specify exactly one argument, a file to load as input");
+        bail!("must specify exactly one argument, a file to load as input");
     }
     let file_path = &args[1];
     File::open(file_path).context("could not find input file")
+}
+
+pub enum Variant {
+    A,
+    B,
+}
+
+// Use the first argument passed to this binary as the file path to a file containing input data,
+// and the second to bifurcate depending on which of the day's prompts (a or b) is being solved.
+pub fn open_input_file_with_variant() -> Result<(File, Variant)> {
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() != 3 {
+        bail!("Must specify exactly two arguments, a file to load as input, and a variant (a/b)");
+    }
+    let file_path = &args[1];
+    let variant = match args[2].as_str() {
+        "a" | "A" => Variant::A,
+        "b" | "B" => Variant::B,
+        _ => bail!("incorrect variant")
+    };
+    Ok((File::open(file_path).context("could not find input file")?, variant))
 }
--- 2022/day-2/src/main.rs
+++ 2022/day-2/src/main.rs
@@ -1,6 +1,23 @@
 use anyhow::{bail, Context, Result};
-use common::open_input_file;
+use common::{open_input_file_with_variant, Variant};
 use std::io::{self, BufRead};
+
+enum Outcome {
+    Win,
+    Draw,
+    Lose,
+}
+
+impl Outcome {
+    fn from_string(code: &str) -> Result<Outcome> {
+        match code {
+            "Z" => Ok(Outcome::Win),
+            "Y" => Ok(Outcome::Draw),
+            "X" => Ok(Outcome::Lose),
+            _ => bail!("invalid input code"),
+        }
+    }
+}
 
 enum Throw {
     Rock,
@@ -37,25 +54,58 @@ impl Throw {
             },
         }
     }
+
+    fn cheat(&self, want_outcome: Outcome) -> u32 {
+        match self {
+            // To "cheat" against rock, we need to throw paper to win, and scissors to lose.
+            Throw::Rock => match want_outcome {
+                Outcome::Draw => 4,
+                Outcome::Win => 8,
+                Outcome::Lose => 3,
+            },
+            // To "cheat" against paper, we need to throw scissors to win, and rock to lose.
+            Throw::Paper => match want_outcome {
+                Outcome::Draw => 5,
+                Outcome::Win => 9,
+                Outcome::Lose => 1,
+            },
+            // To "cheat" against scissors, we need to throw rock to win, and paper to lose.
+            Throw::Scissors => match want_outcome {
+                Outcome::Draw => 6,
+                Outcome::Win => 7,
+                Outcome::Lose => 2,
+            },
+        }
+    }
 }
 
 fn main() -> Result<()> {
-    let sum =
-        io::BufReader::new(open_input_file()?)
-            .lines()
-            .try_fold(0u32, |acc, line| match line {
-                Ok(line) => {
-                    if !line.is_empty() {
-                        let mut throws = line.as_str().split_whitespace();
-                        let them =
-                            Throw::from_string(throws.next().context("missing their throw")?)?;
-                        let me = Throw::from_string(throws.next().context("missing my throw")?)?;
-                        return Ok(acc + me.versus(them));
-                    }
-                    Ok(acc)
+    let (input, variant) = open_input_file_with_variant()?;
+    let sum = io::BufReader::new(input)
+        .lines()
+        .try_fold(0u32, |acc, line| match line {
+            Ok(line) => {
+                if !line.is_empty() {
+                    let mut strategy = line.as_str().split_whitespace();
+                    let them = Throw::from_string(strategy.next().context("missing their throw")?)?;
+                    return match variant {
+                        Variant::A => {
+                            let me =
+                                Throw::from_string(strategy.next().context("missing my throw")?)?;
+                            Ok(acc + me.versus(them))
+                        }
+                        Variant::B => {
+                            let want_outcome = Outcome::from_string(
+                                strategy.next().context("missing desired outcome")?,
+                            )?;
+                            Ok(acc + them.cheat(want_outcome))
+                        }
+                    };
                 }
-                Err(err) => bail!(err),
-            })?;
+                Ok(acc)
+            }
+            Err(err) => bail!(err),
+        })?;
 
     println!("My cumulative score is: {}", sum);
     Ok(())
