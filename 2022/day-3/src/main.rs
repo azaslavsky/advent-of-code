abe2612b7f9f8b75c326a0abd3da3508d672f312
Alex Zaslavsky <>
2023-01-31 10:26:09 -0800

    [2022#3b] Solve prompt

--- 2022/day-3/src/main.rs
+++ 2022/day-3/src/main.rs
@@ -1,8 +1,11 @@
-use anyhow::{bail, Result};
-use common::open_input_file_with_variant;
-use std::collections::HashSet;
+#![feature(iter_array_chunks)]
+
+use anyhow::{bail, Error, Result};
+use common::{open_input_file_with_variant, Variant};
+use std::collections::{HashMap, HashSet};
 use std::io::{self, BufRead};
 
+// Score a letter by priority.
 fn char_to_priority(ch: char) -> Result<u32> {
     if ch.is_lowercase() {
         // "a" is the decimal value 97 in ASCII; to make it or any other
@@ -17,33 +20,76 @@ fn char_to_priority(ch: char) -> Result<u32> {
     }
 }
 
-fn main() -> Result<()> {
-    let (input, _variant) = open_input_file_with_variant()?;
-    let sum = io::BufReader::new(input)
-        .lines()
-        .try_fold(0u32, |acc, line| match line {
-            Ok(line) => {
-                if !line.is_empty() {
-                    let mut seen = HashSet::<u32>::new();
-                    let count = line.len()/2;
-                    if line.len() % 2 != 0 {
-                        bail!("each rucksack must have an even number of items")
-                    }
+// Find the item common to both the first half and second half of the input list.
+fn get_overlapping_item_priority(line: &str) -> Result<u32> {
+    let mut seen = HashSet::<u32>::new();
+    let count = line.len() / 2;
+    if line.len() % 2 != 0 {
+        bail!("each rucksack must have an even number of items")
+    }
 
-                    for (index, ch) in line.chars().enumerate() {
-                        if index < count {
-                            seen.insert(char_to_priority(ch)?);
-                        } else {
-                            if let Some(priority) = seen.get(&char_to_priority(ch)?) {
-                                return Ok(acc + priority);
-                            }
-                        }
-                    }
+    for (index, ch) in line.chars().enumerate() {
+        if index < count {
+            seen.insert(char_to_priority(ch)?);
+        } else {
+            if let Some(priority) = seen.get(&char_to_priority(ch)?) {
+                return Ok(*priority);
+            }
+        }
+    }
+    bail!("no duplicate item found")
+}
+
+// Find the item that occurs in each of the three lines in the |triplet| provided.
+fn get_common_item_priority(triplet: [Result<String, std::io::Error>; 3]) -> Result<u32> {
+    let mut counts = HashMap::<char, u32>::new();
+    for line in triplet {
+        match line {
+            Ok(line) => {
+                if line.is_empty() {
+                    bail!("encountered empty line")
                 }
-                bail!("no duplicate item found")
+
+                // Use the "seen" set to ensure that we don't count an item multiple times per line.
+                let mut seen = HashSet::<char>::new();
+                line.chars().for_each(|ch| {
+                    if !seen.contains(&ch) {
+                        seen.insert(ch);
+                        counts
+                            .entry(ch)
+                            .and_modify(|count| {
+                                *count += 1;
+                            })
+                            .or_insert(1);
+                    }
+                })
             }
             Err(err) => bail!(err),
-        })?;
+        }
+    }
+    match counts.into_iter().find(|(_, count)| *count == 3) {
+        Some((ch, _)) => char_to_priority(ch),
+        None => bail!("no common item found"),
+    }
+}
+
+fn main() -> Result<()> {
+    let (input, variant) = open_input_file_with_variant()?;
+    let mut lines = io::BufReader::new(input).lines();
+    let sum = match variant {
+        Variant::A => lines.try_fold(0u32, |acc, line| match line {
+            Ok(line) => {
+                if !line.is_empty() {
+                    return Ok(acc + get_overlapping_item_priority(line.as_str())?);
+                }
+                bail!("encountered empty line")
+            }
+            Err(err) => bail!(err),
+        })?,
+        Variant::B => lines.array_chunks::<3>().try_fold(0u32, |acc, triplet| {
+            return Ok::<u32, Error>(acc + get_common_item_priority(triplet)?);
+        })?,
+    };
 
     println!("The cumulative priority value is: {}", sum);
     Ok(())
