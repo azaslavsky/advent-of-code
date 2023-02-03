c4498702795d5847352b6a8e67e344e9a62a04a5
Alex Zaslavsky <>
2023-02-02 22:13:09 -0800

    [2022#10b] Solve prompt

--- 2022/day-10/src/main.rs
+++ 2022/day-10/src/main.rs
@@ -1,12 +1,17 @@
 use anyhow::{bail, Result};
-use common::get_input_file_lines_with_variant;
+use common::{get_input_file_lines_with_variant, Variant};
 
-const START_A: usize = 20;
-const STOP_A: usize = 220;
-const GAP_A: usize = 40;
+// Which cycle to start sampling at.
+const START: usize = 20;
+
+// Which cycle to stop the processor at.
+const STOP: usize = 240;
+
+// The gap between samples (and also, conveniently, the width of the screen in pixels).
+const GAP: usize = 40;
 
 struct Processor<const START: usize, const STOP: usize, const GAP: usize> {
-    completed_cycles: usize,
+    cycles: usize,
     signal: isize,
     samples: Vec<isize>,
 }
@@ -24,29 +29,45 @@ enum Instruction {
 impl<const START: usize, const STOP: usize, const GAP: usize> Processor<START, STOP, GAP> {
     fn new() -> Processor<START, STOP, GAP> {
         Processor::<START, STOP, GAP> {
-            completed_cycles: 0,
+            cycles: 0,
             signal: 1,
             samples: vec![],
         }
     }
 
-    fn noop(&mut self) -> Continue {
-        self.increment_then_sample(1)
+    fn noop<F>(&mut self, f: &mut F) -> Result<Continue>
+    where
+        F: FnMut(usize, isize) -> Result<()>,
+    {
+        self.increment_then_sample(1, f)
     }
 
-    fn add(&mut self, diff: isize) -> Continue {
-        let keep_going = self.increment_then_sample(2);
+    fn add<F>(&mut self, diff: isize, f: &mut F) -> Result<Continue>
+    where
+        F: FnMut(usize, isize) -> Result<()>,
+    {
+        let keep_going = self.increment_then_sample(2, f);
         self.signal += diff;
         keep_going
     }
 
-    fn increment_then_sample(&mut self, inc: usize) -> Continue {
-        self.completed_cycles += inc;
-        let want_samples = (self.completed_cycles + START) / GAP;
+    fn increment_then_sample<F>(&mut self, inc: usize, f: &mut F) -> Result<Continue>
+    where
+        F: FnMut(usize, isize) -> Result<()>,
+    {
+        for _ in 0..inc {
+            self.cycles += 1;
+            f(self.cycles, self.signal)?;
+        }
+        let want_samples = (self.cycles + START) / GAP;
         if self.samples.len() < want_samples {
             self.samples.push(self.signal);
         }
-        if self.completed_cycles > STOP {
+        Ok(self.keep_going())
+    }
+
+    fn keep_going(&self) -> Continue {
+        if self.cycles > STOP {
             return Continue::No;
         }
         Continue::Yes
@@ -69,27 +90,72 @@ fn parse_line(line: &str) -> Result<Instruction> {
     }
 }
 
-fn main() -> Result<()> {
-    let (lines, _variant) = get_input_file_lines_with_variant()?;
-
-    let mut processor = Processor::<START_A, STOP_A, GAP_A>::new();
-    for line in lines {
-        let keep_going = match parse_line(line.as_str())? {
-            Instruction::Noop => processor.noop(),
-            Instruction::Add(diff) => processor.add(diff),
-        };
-        if let Continue::No = keep_going {
-            break;
+fn render<const TOTAL: usize, const WIDTH: usize>(pixels: [bool; TOTAL]) {
+    let mut row = String::new();
+    for (index, is_set) in pixels.iter().enumerate() {
+        if index % WIDTH == 0 {
+            println!("{}", row);
+            row = String::new();
         }
-    }
-
-    let sum = processor
-        .samples
-        .iter()
-        .enumerate()
-        .fold(0isize, |acc, (index, signal)| {
-            acc + ((START_A as isize + (index * GAP_A) as isize) * signal)
+        row.push(match is_set {
+            true => '#',
+            false => '.',
         });
-    println!("The sum of the strengths at the sample points is: {}", sum);
+    }
+    println!("{}", row);
+}
+
+fn main() -> Result<()> {
+    let (lines, variant) = get_input_file_lines_with_variant()?;
+
+    let mut processor = Processor::<START, STOP, GAP>::new();
+    match variant {
+        Variant::A => {
+            let mut handler = |_, _| Ok(());
+            for line in lines {
+                let keep_going = match parse_line(line.as_str())? {
+                    Instruction::Noop => processor.noop(&mut handler),
+                    Instruction::Add(diff) => processor.add(diff, &mut handler),
+                };
+                if let Continue::No = keep_going? {
+                    break;
+                }
+            }
+
+            println!(
+                "The combination of the strengths at the sample points is: {}",
+                processor
+                    .samples
+                    .iter()
+                    .enumerate()
+                    .fold(0isize, |acc, (index, signal)| {
+                        acc + ((START as isize + (index * GAP) as isize) * signal)
+                    })
+            );
+        }
+        Variant::B => {
+            let mut pixels = [false; STOP];
+            let mut handler = |cycle, signal: isize| {
+                let pos: usize = cycle % GAP;
+                let offset: isize = pos.try_into()?;
+                if offset >= signal && offset <= signal + 2 {
+                    pixels[cycle - 1] = true
+                } else {
+                    pixels[cycle - 1] = false
+                }
+                Ok(())
+            };
+            for line in lines {
+                let keep_going = match parse_line(line.as_str())? {
+                    Instruction::Noop => processor.noop(&mut handler),
+                    Instruction::Add(diff) => processor.add(diff, &mut handler),
+                };
+                if let Continue::No = keep_going? {
+                    break;
+                }
+            }
+            render::<STOP, GAP>(pixels);
+        }
+    };
     Ok(())
 }
