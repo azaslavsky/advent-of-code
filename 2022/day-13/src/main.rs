e9c57d7f1ca15cdaa46ccdc7fa7681d51d4f8136
Alex Zaslavsky <>
2023-02-04 08:09:27 -0800

    [2022#13b] Solve prompt

--- 2022/day-13/src/main.rs
+++ 2022/day-13/src/main.rs
@@ -1,11 +1,11 @@
 use anyhow::{bail, Context, Result};
-use common::get_input_file_lines_with_variant;
+use common::{get_input_file_lines_with_variant, Variant};
 use std::{
     cmp::{min, Ordering, PartialOrd},
     str::Chars,
 };
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Ord)]
 enum Item {
     Num(Integer),
     List(List),
@@ -31,7 +31,7 @@ impl PartialOrd for Item {
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord)]
 struct Integer {
     value: u32,
 }
@@ -50,7 +50,7 @@ impl PartialOrd for Integer {
     }
 }
 
-#[derive(Debug, Default, PartialEq)]
+#[derive(Debug, Default, Eq, PartialEq, Ord)]
 struct List {
     items: Vec<Item>,
 }
@@ -117,6 +117,27 @@ fn parse_list(chars: &mut Chars) -> Result<List> {
     bail!("parsing: all open brackets should have a matching closing pair")
 }
 
+fn parse_line(line: &str) -> Result<List> {
+    let mut chars = line.chars();
+    if chars
+        .next()
+        .context("parsing: list line with no opening character")?
+        != '['
+    {
+        bail!("parsing: list line with no opening bracket")
+    }
+    Ok(parse_list(&mut chars)?)
+}
+
+fn parse_all(lines: Vec<String>) -> Result<Vec<List>> {
+    lines.iter().try_fold(Vec::new(), |mut acc, line| {
+        if !line.is_empty() {
+            acc.push(parse_line(line.as_str())?);
+        }
+        Ok(acc)
+    })
+}
+
 fn compare_lists(left: &Option<List>, right: &Option<List>) -> Result<bool> {
     match &left {
         None => bail!("parsing: zero lists provided for comparison"),
@@ -133,7 +154,7 @@ fn compare_lists(left: &Option<List>, right: &Option<List>) -> Result<bool> {
     }
 }
 
-fn compare_all(lines: Vec<String>) -> Result<Vec<Option<usize>>> {
+fn parse_and_compare_all(lines: Vec<String>) -> Result<Vec<Option<usize>>> {
     let mut left = None;
     let mut right = None;
     let mut results = lines
@@ -148,16 +169,7 @@ fn compare_all(lines: Vec<String>) -> Result<Vec<Option<usize>>> {
                 left = None;
                 right = None;
             } else {
-                let mut chars = line.chars();
-                if chars
-                    .next()
-                    .context("parsing: list line with no opening character")?
-                    != '['
-                {
-                    bail!("parsing: list line with no opening bracket")
-                }
-
-                let entry = parse_list(&mut chars)?;
+                let entry = parse_line(line.as_str())?;
                 if left.is_none() {
                     left = Some(entry);
                 } else if right.is_none() {
@@ -179,14 +191,51 @@ fn compare_all(lines: Vec<String>) -> Result<Vec<Option<usize>>> {
 }
 
 fn main() -> Result<()> {
-    let (lines, _variant) = get_input_file_lines_with_variant()?;
-    let sum = compare_all(lines)?
-        .iter()
-        .fold(0, |acc, maybe_val| acc + maybe_val.unwrap_or(0));
+    let (lines, variant) = get_input_file_lines_with_variant()?;
 
-    println!(
-        "The sum of the indices of the correctly ordered pairs is: {}",
-        sum
-    );
+    match variant {
+        Variant::A => println!(
+            "The sum of the indices of the correctly ordered pairs is: {}",
+            parse_and_compare_all(lines)?
+                .iter()
+                .fold(0, |acc, maybe_val| acc + maybe_val.unwrap_or(0))
+        ),
+        Variant::B => {
+            let mut parsed = parse_all(lines)?;
+            parsed.push(List {
+                items: vec![Item::List(List {
+                    items: vec![Item::Num(Integer { value: 2 })],
+                })],
+            });
+            parsed.push(List {
+                items: vec![Item::List(List {
+                    items: vec![Item::Num(Integer { value: 6 })],
+                })],
+            });
+            parsed.sort();
+
+            let mut start = None;
+            let mut end = None;
+            for (index, outer) in parsed.iter().enumerate() {
+                if outer.items.len() == 1 {
+                    if let Item::List(inner) = &outer.items[0] {
+                        if inner.items.len() == 1 {
+                            if let Item::Num(int) = &inner.items[0] {
+                                if int.value == 2 {
+                                    start = Some(index + 1)
+                                } else if int.value == 6 {
+                                    end = Some(index + 1)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            println!(
+                "The product of the indices of the divider packets is: {}",
+                start.context("no decoder key start")? * end.context("no decoder key end")?
+            );
+        }
+    }
     Ok(())
 }
